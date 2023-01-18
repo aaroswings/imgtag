9dd90cee4687e3289b6d09bc498abaeb73a68b98
Evan Bell <drummer.bell@gmail.com>
2023-01-17 16:26:50 -0800

    add include files to project

---
+++ include/gui/mwSidebar.hpp
@@ -0,0 +1,13 @@
+#pragma once
+
+#include <QWidget>
+
+/*
+When button is clicked, emit signal to main with button id.
+Change main window to have an instance of this in its member variables.
+
+*/
+
+class MWSidebar : public QWidget
+{
+};
---
+++ include/resource.hpp
@@ -0,0 +1,8 @@
+#pragma once
+
+#include <QFile>
+#include <QIODevice>
+#include <QString>
+#include <QWidget>
+
+void setStyleFromCss(QWidget &widget, const QString &filePath);
